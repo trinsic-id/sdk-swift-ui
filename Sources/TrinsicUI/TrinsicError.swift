3f74a00f624ddf6c4b7440108f03e5805cd2e40b
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-09-12 11:48:08 -0700

    Expose error values as string and improve readme (#3)

--- Sources/TrinsicUI/TrinsicError.swift
+++ Sources/TrinsicUI/TrinsicError.swift
@@ -21,8 +21,39 @@ import Foundation
 @objc public class TrinsicError: NSObject {
     @objc public static func error(with code: TrinsicErrorCode, message: String? = nil) -> NSError {
         let domain = "id.trinsic.TrinsicError"
-        let errorDescription = "\(String(describing: code)): \(message ?? "")"
+        let errorDescription = "\(description(for: code)): \(message ?? "")"                
         let userInfo = [NSLocalizedDescriptionKey: errorDescription]
         return NSError(domain: domain, code: code.rawValue, userInfo: userInfo)
+    }
+    
+    @objc private static func description(for code: TrinsicErrorCode) -> String {
+        switch code {
+        case .unknownError:
+            return "Unknown error occurred."
+        case .unparsableResultUrl:
+            return "The result URL could not be parsed."
+        case .couldNotAcquireRootViewController:
+            return "Could not acquire the root view controller."
+        case .unsupportedIosVersion:
+            return "Unsupported iOS version."
+        case .noRegisteredApplicationForLaunchUrl:
+            return "No registered application for the launch URL."
+        case .unparsableLaunchUrl:
+            return "The launch URL could not be parsed."
+        case .missingSessionId:
+            return "The session ID is missing."
+        case .noSupportForiOSBelow12:
+            return "This feature is not supported on iOS below version 12."
+        case .emptyLaunchUrl:
+            return "The launch URL is empty."
+        case .emptyRedirectUrl:
+            return "The redirect URL is empty."
+        case .cannotReconstructLaunchUrl:
+            return "Cannot reconstruct the launch URL."
+        case .unparsableCallbackUrl:
+            return "The callback URL could not be parsed."
+        case .unsupportedHttpsLinking:
+            return "HTTPS linking is not supported."
+        }
     }
 }
